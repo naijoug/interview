f62e582298dde2e00b6bfd075e9e141a680b8bb5
naijoug <naijoug@outlook.com>
2025-01-03 00:20:29 +0800

    fix: gh action

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -10,10 +10,10 @@ const config: Config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://naijoug.github.io/interview',
+  url: 'https://naijoug.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/interview',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
